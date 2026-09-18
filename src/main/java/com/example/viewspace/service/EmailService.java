933df8d2f49ba687276795fc62c63e2c97daf06e
Neena Soni <sonineena17@gmail.com>
2026-09-18 16:34:48 +0530

    change 1

--- src/main/java/com/example/viewspace/service/EmailService.java
+++ src/main/java/com/example/viewspace/service/EmailService.java
@@ -19,7 +19,7 @@ public class EmailService {
 	
 	private final JavaMailSender mailSender;
 	
-	@Value("${spring.mail.username}")
+	@Value("${emails.sender_email}")
 	private String fromEmail;
 			
 //	public void sendMail(String to, String otp)
